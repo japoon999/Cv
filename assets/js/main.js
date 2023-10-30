d446b0bebd567465240439647a778238a0508715
japoon999 <aziz.azizli.95@inbox.ru>
2023-10-30 17:25:19 +0400

    created cv

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let name = "Aziz";
+let surname = "Azizli";
+let age = 29;
+let myJob = " Mudafie Senayesi Nazirliyi"
+
+document.write("Name:" + name);
+document.write("<br>");
+document.write("Last Name:" + surname);
+document.write("<br>");
+document.write("Age:" + age);
+document.write("<br>");
+document.write("Job:" + myJob)
